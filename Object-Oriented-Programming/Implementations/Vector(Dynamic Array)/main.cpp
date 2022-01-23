c3a3331e60800b45d4c086799ca6f5faaed99841
Aleks Bozhinov <alexbozhinov2000@gmail.com>
2022-01-23 18:36:15 +0200

    commit

---
+++ Object-Oriented-Programming/Implementations/Vector(Dynamic Array)/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include "MyArray.h"
+#include "MyArray.cpp"
+#include "animals.cpp"
+
+using namespace std;
+
+int main(){
+    /*
+MyArray<int> arr;
+arr.addBack(1);
+arr.addBack(2);
+arr.addBack(3);
+arr.addBack(4);
+arr.addBack(5);
+arr.addBack(6);
+arr.addBack(7);
+arr.addBack(8);
+arr.addBack(9);
+arr.addBack(10);
+arr.removeBack();
+for (unsigned int i = 0; i < arr.getSize(); i++)
+		cout << ' ' << arr[i];
+cout<<endl;
+cout<<arr.getElementAt(3)<<endl;
+
+arr.addElementAt(4,12);
+cout<<arr.getElementAt(4)<<endl;
+
+for (unsigned int i = 0; i < arr.getSize(); i++)
+		cout << ' ' << arr[i];
+        cout<<endl;
+arr.removeElementAt(4);
+cout<<arr.getElementAt(4)<<endl;
+
+for (unsigned int i = 0; i < arr.getSize(); i++)
+		cout << ' ' << arr[i];*/
+   MyArray<Animal> animals;
+}
